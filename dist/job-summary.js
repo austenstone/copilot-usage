3a7d72eddbb81126ddb8ef0a18cde2a46da53ed1
Austen Stone <austenstone@github.com>
2024-05-09 14:02:29 -0400

    refactor: Update seat.last_activity_editor fallback value to 'N/A'

--- dist/job-summary.js
+++ dist/job-summary.js
@@ -92,28 +92,6 @@ export const createJobSummarySeatInfo = (data) => {
 export const createJobSummarySeatAssignments = (data) => {
     if (!data)
         data = [];
-    console.log([
-        [
-            { data: 'Avatar', header: true },
-            { data: 'Login', header: true },
-            { data: 'Last Activity', header: true },
-            { data: 'Last Editor Used', header: true },
-            { data: 'Created At', header: true },
-            { data: 'Updated At', header: true },
-            { data: 'Pending Cancellation Date', header: true },
-            { data: 'Team', header: true },
-        ],
-        ...data.map(seat => [
-            `<img src="${seat.assignee?.avatar_url}" width="33" />`,
-            seat.assignee?.login,
-            seat.last_activity_at ? dateFormat(seat.last_activity_at) : 'No Activity',
-            seat.last_activity_editor || 'N/A',
-            dateFormat(seat.created_at),
-            dateFormat(seat.updated_at),
-            dateFormat(seat.pending_cancellation_date) || ' ',
-            String(seat.assigning_team?.name || ' '),
-        ])
-    ]);
     return summary
         .addHeading('Seat Assignments')
         .addTable([
@@ -131,7 +109,7 @@ export const createJobSummarySeatAssignments = (data) => {
             `<img src="${seat.assignee?.avatar_url}" width="33" />`,
             seat.assignee?.login,
             seat.last_activity_at ? dateFormat(seat.last_activity_at) : 'No Activity',
-            seat.last_activity_editor,
+            seat.last_activity_editor || 'N/A',
             dateFormat(seat.created_at),
             dateFormat(seat.updated_at),
             dateFormat(seat.pending_cancellation_date) || ' ',
