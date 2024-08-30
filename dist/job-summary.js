2da7b0b7f6f99208ffbdfe94e647b16b0fba3321
Austen Stone <austenstone@github.com>
2024-08-30 10:33:48 -0400

    Refactor sorting of orgSeatAssignments in run.ts to use last_activity_at instead of updated_at

--- dist/job-summary.js
+++ dist/job-summary.js
@@ -155,7 +155,7 @@ const getTableWeeklyUsage = (data) => {
         d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
         d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
         const yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
-        const weekNo = Math.ceil((((d - yearStart) / 86400000) + 1) / 7);
+        const weekNo = Math.ceil((((d - Number(yearStart)) / 86400000) + 1) / 7);
         return [d.getUTCFullYear(), weekNo];
     };
     return [
--- src/job-summary.ts
+++ src/job-summary.ts
@@ -179,11 +179,11 @@ const getTableDailyUsage = (data: CopilotUsageResponse) => {
 
 // get weekly table that totals the data for each week
 const getTableWeeklyUsage = (data: CopilotUsageResponse) => {
-  const getWeekNumber = (d: any) => {
+  const getWeekNumber = (d) => {
     d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
     d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
     const yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
-    const weekNo = Math.ceil((((d - yearStart) / 86400000) + 1) / 7);
+    const weekNo = Math.ceil((((d - Number(yearStart)) / 86400000) + 1) / 7);
     return [d.getUTCFullYear(), weekNo];
   }
   return [
